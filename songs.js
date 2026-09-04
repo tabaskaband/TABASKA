ee04f808d69c28b1524b17ead04820a190d545ad
tabaskaband <tabaska.band@gmail.com>
2026-09-04 17:13:07 +0900

    Replace existing songs with a test song entry

--- songs.js
+++ songs.js
@@ -1,23 +1,7 @@
-//TEST
-
 const songs = [
-
   {
-    title: "未来へ",
-    artist: "Kiroro",
-    lyrics: "https://www.uta-net.com/song/14476/"
-  },
-
-  {
-    title: "糸",
-    artist: "中島みゆき",
-    lyrics: "https://www.uta-net.com/song/2656/"
-  },
-
-  {
-    title: "ハナミズキ",
-    artist: "一青窈",
-    lyrics: "https://www.uta-net.com/song/17878/"
+    title: "テスト曲",
+    artist: "TABASKA",
+    lyrics: "https://www.google.com"
   }
-
 ];
