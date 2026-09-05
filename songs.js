3de61b2d65e82b1b3ea79ee710c793a59a51a79e
tabaskaband <tabaska.band@gmail.com>
2026-09-05 09:31:20 +0900

    Update songs.js with October stage setlists and lyric links

--- songs.js
+++ songs.js
@@ -5,18 +5,124 @@ const concertDate = "2026年◯◯月◯◯日（土）";
 const concertTitle = "TABASKA LIVE";
 const concertVenue = "○○○○";
 const concertTime = "**:**";
+
 // ========================================
 // 曲一覧
 // ========================================
 const songs = [
+  // ========================================
+  // STAGE 1
+  // ========================================
   {
-    title: "１",
-    artist: "１",
-    lyrics: "歌詞サイトのURL"
+    stage: "STAGE 1",
+    title: "昭和歌謡曲メドレー② カモメ",
+    artist: "昭和歌謡",
+    lyrics: ""
   },
   {
-    title: "２",
-    artist: "２",
-    lyrics: "歌詞サイトのURL"
+    stage: "STAGE 1",
+    title: "おジャ魔女カーニバル!!",
+    artist: "MAHO堂",
+    lyrics: "https://www.uta-net.com/song/14341/"
+  },
+  {
+    stage: "STAGE 1",
+    title: "勇気と冒険のメドレー",
+    artist: "",
+    lyrics: ""
+  },
+  {
+    stage: "STAGE 1",
+    title: "A Whole New World",
+    artist: "Disney",
+    lyrics: "https://www.uta-net.com/song/76296/"
+  },
+  {
+    stage: "STAGE 1",
+    title: "優しい彗星",
+    artist: "YOASOBI",
+    lyrics: "https://www.uta-net.com/song/296725/"
+  },
+  {
+    stage: "STAGE 1",
+    title: "逢いたくていま",
+    artist: "MISIA",
+    lyrics: "https://www.oricon.co.jp/prof/17265/lyrics/171078/"
+  },
+  {
+    stage: "STAGE 1",
+    title: "夢をあきらめないで",
+    artist: "岡村孝子",
+    lyrics: "https://www.uta-net.com/song/4712/"
+  },
+  {
+    stage: "STAGE 1",
+    title: "情熱大陸",
+    artist: "葉加瀬太郎",
+    lyrics: ""
+  },
+  {
+    stage: "STAGE 1",
+    title: "ふるさと",
+    artist: "童謡・唱歌",
+    lyrics: "https://www.uta-net.com/song/13892/"
+  },
+  {
+    stage: "STAGE 1",
+    title: "昭和歌謡曲メドレー⑤",
+    artist: "昭和歌謡",
+    lyrics: ""
+  },
+
+  // ========================================
+  // STAGE 2
+  // ========================================
+  {
+    stage: "STAGE 2",
+    title: "アンパンマンメドレー",
+    artist: "アンパンマン",
+    lyrics: ""
+  },
+  {
+    stage: "STAGE 2",
+    title: "パウ・パトロール／ジブリメドレー",
+    artist: "パウ・パトロール／ジブリ",
+    lyrics: ""
+  },
+  {
+    stage: "STAGE 2",
+    title: "もののけ姫",
+    artist: "米良美一",
+    lyrics: "https://www.uta-net.com/song/10104/"
+  },
+  {
+    stage: "STAGE 2",
+    title: "恋風",
+    artist: "幾田りら",
+    lyrics: "https://www.uta-net.com/song/371553/"
+  },
+  {
+    stage: "STAGE 2",
+    title: "威風堂々 Brass Rock",
+    artist: "Edward Elgar",
+    lyrics: ""
+  },
+  {
+    stage: "STAGE 2",
+    title: "元気を出して",
+    artist: "竹内まりや",
+    lyrics: "https://www.uta-net.com/song/1769/"
+  },
+  {
+    stage: "STAGE 2",
+    title: "サザンメドレー",
+    artist: "サザンオールスターズ",
+    lyrics: ""
+  },
+  {
+    stage: "STAGE 2",
+    title: "時代",
+    artist: "中島みゆき",
+    lyrics: "https://www.uta-net.com/song/2416/"
   }
 ];
