33d291769b19d1318dfc38ec8c6881440aa44435
tabaskaband <tabaska.band@gmail.com>
2026-09-06 18:04:07 +0900

    Add lyrics availability date to songs.js

--- songs.js
+++ songs.js
@@ -11,7 +11,7 @@ const concertVenue = "イオンモールKagoshima BAY";
 // ステージ別 開演時間
 const stage1Time = "15:30";
 const stage2Time = "17:00";
-
+const lyricsAvailableUntil = "2026-10-04";
 
 // ========================================
 // 曲一覧
