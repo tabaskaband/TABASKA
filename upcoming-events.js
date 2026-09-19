1adf38db2b4501b08fe09933adab8aea079f7f8c
tabaskaband <tabaska.band@gmail.com>
2026-09-19 16:09:26 +0900

    Update upcoming-events.js

--- upcoming-events.js
+++ upcoming-events.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
     videoSection.innerHTML = `
       <h2>TABASKAを動画で紹介</h2>
       <p style="text-align:center; margin-bottom:20px; color:#94a3b8;">
-        「笑って。」「泣いて。」「踊って。」「歌って。」　音楽で、みんながひとつになる。<br>
+        音楽で、みんながひとつになる。<br>
         
       </p>
       <style>
